c57abc112d8bd2eb11b324afb5a1ddc077357567
Necropixel15616 <jacob.tuckwell@outlook.com>
2022-11-04 19:52:12 +1100

    Create Entry Entity and Dto

---
+++ TomCatRaffleProgram.Program/ApplicationLayer/Dtos/EntryDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TomCatRaffleProgram.Program.Domain.Entities;
+
+namespace TomCatRaffleProgram.Program.ApplicationLayer.Dtos
+{
+    class EntryDto
+    {
+        public EntryDto() { }
+        public EntryDto(Entry _entry) 
+        {
+            this.FirstName = _entry.FirstName;
+            this.LastName = _entry.LastName;
+            this.Tickets = _entry.Tickets;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Tickets { get; set; }
+
+    }
+}
