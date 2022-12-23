cac2a9fa08aaf345c6c847cdf5efed3276eeac93
Necropixel15616 <jacob.tuckwell@outlook.com>
2022-12-23 12:03:09 +1100

    Commit GetrafflesPresenter

---
+++ TomCatRaffleProgram.Program/Framework/Presentation/GetRaffles/GetRafflesPresenter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TomCatRaffleProgram.Program.ApplicationLayer.Dtos;
+using TomCatRaffleProgram.Program.ApplicationLayer.UseCases.Raffles.GetRaffles;
+using TomCatRaffleProgram.Program.Framework.Presentation.Common;
+
+namespace TomCatRaffleProgram.Program.Framework.Presentation.GetRaffles
+{
+    class GetRafflesPresenter : BasePresenter<List<RaffleViewModel>>, IGetRafflesOutputPort
+    {
+        Task IGetRafflesOutputPort.PresentRaffles(List<RaffleDto> raffleDtos)
+        {
+            var viewModels = new List<RaffleViewModel>();
+            raffleDtos.ForEach(r => viewModels.Add(new RaffleViewModel(r)));
+            return SetResult(viewModels);
+        }
+    }
+}
